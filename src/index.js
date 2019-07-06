25d81ea365216bc87f821241f0489545df0efec5
Dilip Mamidela <dilip.mamidela@sap.com>
2019-07-06 19:28:06 +0530

    try

--- src/index.js
+++ src/index.js
@@ -9,3 +9,27 @@ import "@ui5/webcomponents/dist/Title.js";
 import "@ui5/webcomponents/dist/Select.js";
 import "@ui5/webcomponents/dist/Option.js";
 import "@ui5/webcomponents/dist/Table.js";
+
+const getBt = document.getElementById("getBt");
+getBt.addEventListener("press", () => {
+  var data = "test";
+  var xhr = new XMLHttpRequest();
+  // xhr.withCredentials = false;
+  xhr.addEventListener("readystatechange", function() {
+    if (this.readyState === this.DONE && this.status == 200) {
+      console.log(this.reponseText);
+      var data = JSON.parse(this.responseText).d.results;
+      var arrObj = data;
+      var arrLen = arrObj.length;
+      alert(JSON.stringify(data));
+    }
+    xhr.open(
+      "GET",
+      "https://myhanai065970trial.hanatrial.ondemand.com/sap/hana/xs/admin/appMyOdata/services.xsodata/myODataURL5?$format=json",
+      true
+    );
+    xhr.setRequestHeader("x-csrf-token", "Fetch");
+    xhr.setRequestHeader("Authorization", "Basic TUFNSUQ6QmxyMTIzNDUq");
+    xhr.send(data);
+  });
+});
