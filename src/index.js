7fcd58a960fc60d1e1750d21c53de7dcd7b4d6c3
Dilip Mamidela <dilip.mamidela@sap.com>
2019-07-06 18:38:58 +0530

    try7

--- src/index.js
+++ src/index.js
@@ -8,3 +8,4 @@ import "@ui5/webcomponents/dist/Switch.js";
 import "@ui5/webcomponents/dist/Title.js";
 import "@ui5/webcomponents/dist/Select.js";
 import "@ui5/webcomponents/dist/Option.js";
+import "@ui5/webcomponents/dist/Table";
