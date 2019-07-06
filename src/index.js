f1aa1a31fea4e334bc0c94f9c5974c431756c10a
Dilip Mamidela <dilip.mamidela@sap.com>
2019-07-06 17:59:25 +0530

    try2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import "@ui5/webcomponents/dist/Button.js";
 import "@ui5/webcomponents/dist/Label.js";
-import "@ui5/webcomponents/dist/DatePicker.js";
+import "@ui5/webcomponents/dist/MessageBundleAssets.js";
+import "@ui5/webcomponents/dist/DatePicker";
 import "@ui5/webcomponents/dist/MessageStrip.js";
 import "@ui5/webcomponents/dist/Dialog.js";
 import "@ui5/webcomponents/dist/Switch.js";
 import "@ui5/webcomponents/dist/Title.js";
-
-const openBtn = document.getElementById("openDialogButton");
-const closeBtn = document.getElementById("closeDialogButton");
-const dialog = document.getElementById("hello-dialog");
-openBtn.addEventListener("press", () => {
-  dialog.open();
-});
-closeBtn.addEventListener("press", () => {
-  dialog.close();
-});
+import "@ui5/webcomponents/dist/Select.js";
